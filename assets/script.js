ebecb26f06a2f3e43fbac3b4667ba493b2854a8b
PsychoRSS <Bobgonzalez516@gmail.com>
2023-07-07 02:35:15 -0600

    added a time interval and getting ready to attempt making the game

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,33 @@
-var car
-var yu
+var timerNum = 60;
+var highscore = 0;
+var question = [
+    "What is 2+2",
+    "Who are the founding fathers?",
+    "What is your meaning of life?"
+];
+
+var timerHtml = document.querySelector('#timer');
+var highscoreButton = document.querySelector('#highscore')
+var bodyHtml = document.querySelector('body')
+function timer() {
+    var timerInterval = setInterval(function() {
+    timerNum--;
+    timerHtml.textContent = timerNum + " seconds left till quiz is over.";
+  
+      if(timerNum === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  };
+  
+  
+
+ function game() {
+
+  };
+
+ highscoreButton.addEventListener('click', function () {
+    bodyHtml.hidden
+ })
+
